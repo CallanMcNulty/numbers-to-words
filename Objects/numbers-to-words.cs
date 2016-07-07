25508418c7d49d71c8d305140656a92d07e4a428
Ben Logue and Callan McNulty <ben+callan@>
2016-07-07 14:45:53 -0700

    Add functionality for numbers past two digits

--- Objects/numbers-to-words.cs
+++ Objects/numbers-to-words.cs
@@ -6,7 +6,7 @@ namespace NumberToWord
   public class Converter
   {
     private Dictionary<char, string> _digitNames = new Dictionary<char, string>() {
-                                                                                {'0', "zero"},
+                                                                                {'0', ""},
                                                                                 {'1', "one"},
                                                                                 {'2', "two"},
                                                                                 {'3', "three"},
@@ -30,14 +30,114 @@ namespace NumberToWord
                                                                                 {'8', "eighty"},
                                                                                 {'9', "ninety"}
                                                                                 };
-    public string numberToString(int number)
+
+    private Dictionary<char, string> _teensNames = new Dictionary<char, string>() {
+                                                                                {'0', "ten"},
+                                                                                {'1', "eleven"},
+                                                                                {'2', "twelve"},
+                                                                                {'3', "thirteen"},
+                                                                                {'4', "fourteen"},
+                                                                                {'5', "fifteen"},
+                                                                                {'6', "sixteen"},
+                                                                                {'7', "seventeen"},
+                                                                                {'8', "eightteen"},
+                                                                                {'9', "nineteen"}
+                                                                                };
+
+    private Dictionary<int, string> _placeNames = new Dictionary<int, string>() {
+                                                                                {0, ""},
+                                                                                {1, " thousand"},
+                                                                                {2, " million"},
+                                                                                {3, " billion"},
+                                                                                {4, " trillion"}
+                                                                                };
+
+    public string numberToString(float number)
     {
       string numberString = number.ToString();
+      int decimalIndex = numberString.IndexOf(".");
+      if(decimalIndex==-1)
+      {
+        return manyDigitsToString(numberString);
+      }
+      else
+      {
+        string finalWord = "";
+        finalWord = finalWord + manyDigitsToString(numberString.Substring(0,decimalIndex)) +" and "+ manyDigitsToString(numberString.Substring(decimalIndex+1,numberString.Length-decimalIndex-1));
+        string fraction = "1";
+        for(int i=0; i<numberString.Length-decimalIndex-1; i++)
+        {
+          fraction = fraction + "0";
+        }
+        fraction = manyDigitsToString(fraction);
+        if(fraction.Substring(0,3)=="one")
+        {
+          fraction = fraction.Substring(4,fraction.Length-4);
+        }
+        finalWord = finalWord + " " +  fraction + "ths";
+
+        return finalWord;
+      }
+    }
+    public string manyDigitsToString(string numberString)
+    {
+      string finalWord = "";
+      if(numberString.Length <= 3)
+      {
+        return threeDigitsToString(numberString);
+      }
+      for(int i=0; i<=numberString.Length; i+=3)
+      {
+        int startIndex = Math.Max(0,numberString.Length-i-3);
+        int substringLength = Math.Min(3,numberString.Length-i);
+        string spacer = "";
+        if(finalWord != "")
+        {
+          spacer = " ";
+        }
+        finalWord = threeDigitsToString(numberString.Substring(startIndex,substringLength)) + _placeNames[i/3] +spacer+ finalWord;
+      }
+      return finalWord;
+    }
+
+    public string threeDigitsToString(string numberString)
+    {
       while(numberString.Length < 3)
       {
         numberString = "0" + numberString;
       }
-      return _tensNames[numberString[numberString.Length - 2]] + _digitNames[numberString[numberString.Length - 1]];
+      string finalWord = "";
+      char hundreds = numberString[0];
+      if(hundreds != '0')
+      {
+        finalWord = finalWord + _digitNames[hundreds] + " hundred";
+      }
+      char tens = numberString[1];
+      string spacer = "";
+      if(finalWord != "")
+      {
+        spacer = " ";
+      }
+      if(tens=='1')
+      {
+        finalWord = finalWord +spacer+ _teensNames[numberString[2]];
+      }
+      else
+      {
+        if(_tensNames[tens]=="")
+        {
+          spacer = "";
+        }
+        finalWord = finalWord +spacer+ _tensNames[tens];
+        string ones = _digitNames[numberString[2]];
+        string spacer2 = "";
+        if(ones != "" && finalWord != "")
+        {
+          spacer2 = " ";
+        }
+        finalWord = finalWord + spacer2 + ones;
+      }
+      return  finalWord;
     }
   }
 }
--- Tests/number-to-word-test.cs
+++ Tests/number-to-word-test.cs
@@ -13,7 +13,52 @@ namespace NumberToWord
     [Fact]
     public void Convert_43_fortythree() {
       Converter convert = new Converter();
-      Assert.Equal("fortythree", convert.numberToString(43));
+      Assert.Equal("forty three", convert.numberToString(43));
+    }
+    [Fact]
+    public void Convert_13_thirteen() {
+      Converter convert = new Converter();
+      Assert.Equal("thirteen", convert.numberToString(13));
+    }
+    [Fact]
+    public void Convert_433_fourhundredthirtythree() {
+      Converter convert = new Converter();
+      Assert.Equal("four hundred thirty three", convert.numberToString(433));
+    }
+    [Fact]
+    public void Convert_4433_fourthousandfourhundredthirtythree() {
+      Converter convert = new Converter();
+      Assert.Equal("four thousand four hundred thirty three", convert.numberToString(4433));
+    }
+    [Fact]
+    public void Convert_45433_fortyfivethousandfourhundredthirtythree() {
+      Converter convert = new Converter();
+      Assert.Equal("forty five thousand four hundred thirty three", convert.numberToString(45433));
+    }
+    [Fact]
+    public void Convert_4001433_fourmilliononethousandfourhundredthirtythree() {
+      Converter convert = new Converter();
+      Assert.Equal("four million one thousand four hundred thirty three", convert.numberToString(4001433));
+    }
+    [Fact]
+    public void Convert_400_fourhundred() {
+      Converter convert = new Converter();
+      Assert.Equal("four hundred", convert.numberToString(400));
+    }
+    [Fact]
+    public void Convert_4000_fourthousand() {
+      Converter convert = new Converter();
+      Assert.Equal("four thousand", convert.numberToString(4000));
+    }
+    [Fact]
+    public void Convert_4point3_fourandthreetenths() {
+      Converter convert = new Converter();
+      Assert.Equal("four and three tenths", convert.numberToString(4.3F));
+    }
+    [Fact]
+    public void Convert_4point03_fourandthreehundredths() {
+      Converter convert = new Converter();
+      Assert.Equal("four and three hundredths", convert.numberToString(4.03F));
     }
   }
 }
